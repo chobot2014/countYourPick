0d95a40de9325174fcbe157540326d37fbdd596b
John Chohon <johnchohon@gmail.com>
2016-03-16 14:32:32 +0000

    a;ldkfj

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
 	require 'lol'
-	
-
+	include Lol
 	def sayMyName(summonerName)
 		client = Lol::Client.new "b4b879a7-9939-485f-86e4-b68376d7acc9", {region: "na"}
 		summoner = client.summoner.by_name summonerName
-		return summoner
+	#ranked_stats = client.stats.ranked(summoner.first.id)
+		recent_game = client.game.recent(summoner.first.id)
+		return recent_game
 	end
 
 
