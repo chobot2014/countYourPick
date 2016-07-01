a6962a0cdd4ec10ef222eb7adeecc73be64f5481
John Chohon <johnchohon@gmail.com>
2016-07-01 18:03:29 +0000

    added cypSummoner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,71 @@
 module ApplicationHelper
+	#namespacing
 	require 'lol'
 	include Lol
+	
+	##
+	# Create our summoner objects
+	##
+
+	
+	
+	
+	
+	
+	#
+	# We want to call our rito api stuff as rarely as possible, and get the data	# 
+	# into our own db / datatyping
+	#
+	
+	
+	private def createClientInstance()
+	##remember we can't let anybody see this string..
+		client = Lol::Client.new "b4b879a7-9939-485f-86e4-b68376d7acc9", {region: "na"}
+	end
+	
+	private def get_api_summoner(summonerName, client)
+		returnerSummonerApiObject = client.summoner.by_name(summonerName)
+		return returnerSummonerApiObject
+	end
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##APPLICATION USES
+
+	
 	def sayMyName(summonerName)
 		client = Lol::Client.new "b4b879a7-9939-485f-86e4-b68376d7acc9", {region: "na"}
 		summoner = client.summoner.by_name summonerName
@@ -8,8 +73,5 @@ module ApplicationHelper
 		recent_game = client.game.recent(summoner.first.id)
 		return recent_game
 	end
-
-
 	
-
 end
---
+++ lib/cypSummoner.rb
@@ -0,0 +1,41 @@
+##custom class for our summoner objects
+def class cypSummoner
+
+#declare variables
+
+
+    
+    
+    
+    
+    
+    
+##takes in an API activeRecord summoner
+def new(apiSummoner)
+
+
+
+
+    
+end
+
+#don't fucking use this, but shit breaks without it
+def new()
+
+end
+#get's new info from the api and updates the summoner object -> db
+def refreshData()
+
+
+end
+
+
+##CUSTOM METHODS
+
+    
+    
+    
+    
+	
+	
+end
